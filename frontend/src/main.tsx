98f64861f6fe70087e01be02a6efcb841fb51f32
amirhossein-moloki <amirh.moloki@gmail.com>
2025-09-25 21:39:48 +0330

    Update main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,7 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import 'buffer' // Polyfill for Buffer
+import { Buffer } from 'buffer'
+
+// Polyfill Buffer for use in browser environment (e.g. for things like base64 encoding)
+window.Buffer = Buffer
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
